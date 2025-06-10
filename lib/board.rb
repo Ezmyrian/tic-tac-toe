d19de94dda457d68a055c80ef92f33f106151e82
Ezmyrian <56850288+Ezmyrian@users.noreply.github.com>
2025-06-10 03:06:17 -0500

    Add board as array and display method

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,19 @@
 # Creates board for tic tac toe
-class Board
+class Board < Array
   attr_reader :board
 
   def initialize
-    @board = Array.new(3) { Array.new(3) }
+    super
+    @board = %w[A1 A2 A3 B1 B2 B3 C1 C2 C3]
+  end
+
+  def display(board)
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
 end
+
+# puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+# puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+# puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
