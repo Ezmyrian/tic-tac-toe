200cab5ea14a6336f0b6ee20aa6295a0c56e5ea9
Ezmyrian <56850288+Ezmyrian@users.noreply.github.com>
2026-07-14 07:26:36 -0500

    Add methods to create new player and set mark

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,19 @@
 class Player
-  attr_reader :name
+  def initialize(player_number)
+    @name = name(player_number)
+  end
 
-  def initialize(name)
-    @name = name
+  def name(number)
+    puts "What is the name of player #{number}?"
+    gets.chomp
+  end
+
+  def mark
+    player = ''
+    until (player == 'X') || (player == 'O')
+      puts 'Will player 1 be X or O?'
+      player = gets.chomp.upcase
+    end
+    player
   end
 end
