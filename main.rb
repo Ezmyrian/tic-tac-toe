b79491d00af1c9df24672b828ab42c8d8ad8d61d
Ezmyrian <56850288+Ezmyrian@users.noreply.github.com>
2026-07-14 07:24:05 -0500

    Move methods to the proper files

--- main.rb
+++ main.rb
@@ -4,20 +4,5 @@ require_relative 'lib/board'
 require_relative 'lib/player'
 require_relative 'lib/game'
 
-def get_name(number)
-  puts "What is the name of player #{number}?"
-  gets.chomp
-end
-
-def players
-  player_1 = Player.new(get_name(1))
-  player_2 = Player.new(get_name(2))
-  [player_1, player_2]
-end
-
-def game
-  game = Game.new
-  puts game.game_board.board
-end
-
-game
+game = Game.new
+game.play_game
