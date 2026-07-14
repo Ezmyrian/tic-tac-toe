7f37383e7ac8f3ce9fd7ebc940de88778f51cb4f
Ezmyrian <56850288+Ezmyrian@users.noreply.github.com>
2026-07-14 10:24:44 -0500

    Update method state to display_board_state and print for players to view

--- lib/board.rb
+++ lib/board.rb
@@ -5,7 +5,11 @@ class Board
     @board = [%w[A1 A2 A3], %w[B1 B2 B3], %w[C1 C2 C3]]
   end
 
-  def state
-    @board
+  def display_board_state
+    puts @board[0].join('|')
+    puts '--------'
+    puts @board[1].join('|')
+    puts '--------'
+    puts @board[2].join('|')
   end
 end
