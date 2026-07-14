60d9d756d6d26b62943bec70d0ae697524150e8a
Ezmyrian <56850288+Ezmyrian@users.noreply.github.com>
2026-07-14 07:25:24 -0500

    Add play_game and players to instantiate new players

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,18 @@
 class Game
-  attr_reader :game_board
+  attr_reader :board
 
   def initialize
-    @game_board = Board.new
+    @board = Board.new
+  end
+
+  def play_game
+    players = players()
+    p players
+  end
+
+  def players
+    player_1 = Player.new(1)
+    player_2 = Player.new(2)
+    [player_1, player_2]
   end
 end
