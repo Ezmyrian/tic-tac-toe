9d1b7808191596e68e0daf86f7b917a9d250fb2a
Ezmyrian <56850288+Ezmyrian@users.noreply.github.com>
2026-08-20 17:01:01 -0500

    Change protected to private and move win? and valid_location? to private

--- lib/board.rb
+++ lib/board.rb
@@ -13,11 +13,6 @@ class Board
     puts @board[2].join('|')
   end
 
-  def valid_location?(location)
-    %w[X O].none? { |value| value == location } &&
-      @board.flatten.include?(location)
-  end
-
   def player_move(player)
     location = ''
     until valid_location?(location)
@@ -25,16 +20,6 @@ class Board
       location = gets.chomp.upcase
     end
     update_board(location, player.mark)
-  end
-
-  def win?
-    winning_combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
-                            [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-    flat_board_array = board.flatten
-    winning_combinations.any? do |array|
-      board_values = array.map { |index| flat_board_array[index] }
-      board_values.uniq.length == 1
-    end
   end
 
   def tie?
@@ -46,10 +31,23 @@ class Board
     win? || tie?
   end
 
-  protected
+  private
+
+  def valid_location?(location)
+    %w[X O].none? { |value| value == location } &&
+      @board.flatten.include?(location)
+  end
 
   def update_board(location, mark)
     index = board.flatten.index(location)
     board[index / 3][index % 3] = mark
+  end
+
+  def win?
+    flat_board_array = board.flatten
+    WINNING_COMBINATIONS.any? do |array|
+      board_values = array.map { |index| flat_board_array[index] }
+      board_values.uniq.length == 1
+    end
   end
 end
